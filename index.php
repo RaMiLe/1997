5c8168ebc8040e2d8180fdccad9ff231da7039db
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-22 21:59:42 +0400

    Update index.php

--- index.php
+++ index.php
@@ -26,45 +26,22 @@ border: 0 none; }
 <p>Fill in your name and
 email address, then click <strong>Submit</strong>
 to register.</p>
-<form method="post" action="index.php"
-enctype="multipart/form-data" >
+<form method="post" action="index.php" enctype="multipart/form-data" >
 <input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
 <input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
-<input type ="text" name ="country" id ="country" placeholder ="Страна">
-
-
-<input type ="submit" name ="submit" class ="btn" value ="Отправить">
-
-<input type="submit" name="filter" class="btn" value="Фильтр">
-
-
-
-<input type="submit"
-name="submit" value="Submit" />
+<select name="country">
+<option value="">All</option>
+<option value="Russia">Russia</option>
+<option value="USA">USA</option>
+<option value="Germany">Germany</option>
+<option value="Japan">Japan</option>
+<option value="China">China</option>
+</select>
+<input type ="submit" name ="submit" value ="Отправить">
+<input type="submit" name="filter" value="Фильтр">
 </form>
-<?php
-$dsn = "sqlsrv:server = tcp:rom.database.windows.net,1433; Database = qqq";
-$username = "rom";
-$password = "Rosbank1997";
-try {
-$conn = new PDO($dsn, $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-if(isset($_POST["clear"])) {
-$sql1 = "DELETE FROM registration_on";
-$conn->query($sql1);
-}
-}
-catch (PDOException $e) {
-print("Ошибка подключения к SQL Server.");
-die(print_r($e));
-}
-
-$conn = null;
-
-?>
 
 <?php
-
 $dsn = "sqlsrv:server = tcp:rom.database.windows.net,1433; Database = qqq";
 $username = "rom";
 $password = "Rosbank1997";
@@ -77,29 +54,24 @@ catch (PDOException $e) {
 print("Ошибка подключения к SQL Server.");
 die(print_r($e));
 }
+
 if(!empty($_POST)) {
 try {
 $name = $_POST['name'];
 $email = $_POST['email'];
 $date = date("Y-m-d");
-$gender = $_POST['gender'];
-$age = $_POST['age'];
 $country = $_POST['country'];
-$birthday = $_POST['birthday'];
 
 if ($name == "" || $email == "") {
 echo "<h3>Не заполнены поля name и email.</h3>";
 }
 else {
-$sql_insert ="INSERT INTO registration_on (name, email, date, gender, age, country, birthday) VALUES (?,?,?,?,?,?,?)";
+$sql_insert ="INSERT INTO registration_on (name, email, date, country) VALUES (?,?,?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $name);
 $stmt->bindValue(2, $email);
 $stmt->bindValue(3, $date);
-$stmt->bindValue(4, $gender);
-$stmt->bindValue(5, $age);
-$stmt->bindValue(6, $country);
-$stmt->bindValue(7, $birthday);
+$stmt->bindValue(4, $country);
 $stmt->execute();
 
 echo "<h3>Вы зарегистрировались!</h3>";
@@ -108,35 +80,16 @@ echo "<h3>Вы зарегистрировались!</h3>";
 catch(Exception $e) {
 die(var_dump($e));
 }
-
-}
-
-$conn = null;
-
-?>
-
-<?php
-
-$dsn = "sqlsrv:server = tcp:rom.database.windows.net,1433; Database = qqq";
-$username = "rom";
-$password = "Rosbank1997";
-try {
-$conn = new PDO($dsn, $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-print("Ошибка подключения к SQL Server.");
-die(print_r($e));
 }
 
 $sql_select = "SELECT * FROM registration_on";
 $stmt = $conn->query($sql_select);
 $stmt->execute();
 if(isset($_POST['filter'])) {
-$gender = $_POST['gender'];
-$sql_select = "SELECT * FROM registration_on WHERE gender like :gender";
+$gender = $_POST['country'];
+$sql_select = "SELECT * FROM registration_on WHERE country like :country";
 $stmt = $conn->prepare($sql_select);
-$stmt->execute(array(':gender'=>$gender.'%'));
+$stmt->execute(array(':country'=>$country.'%'));
 }
 $registrants = $stmt->fetchAll();
 if(count($registrants) > 0) {
@@ -144,17 +97,12 @@ echo "<h2>Люди, которые зарегистрированы:</h2>";
 echo "<table>";
 echo "<tr><th>Name</th>";
 echo "<th>Email</th>";
-echo "<th>Gender</th>";
-echo "<th>Age</th>";
 echo "<th>Country</th>";
-echo "<th>Birthday</th>";
 echo "<th>Date</th></tr>";
 foreach($registrants as $registrant) {
 echo "<td>".$registrant['name']."</td>";
 echo "<td>".$registrant['email']."</td>";
-
 echo "<td>".$registrant['country']."</td>";
-
 echo "<td>".$registrant['date']."</td></tr>";
 }
 echo "</table>";
