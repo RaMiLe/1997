792a237dada68ee5d385cb8bb3ad64cb0cf4f2ab
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-22 21:22:24 +0400

    Update index.php

--- index.php
+++ index.php
@@ -28,14 +28,11 @@ email address, then click <strong>Submit</strong>
 to register.</p>
 <form method="post" action="index.php"
 enctype="multipart/form-data" >
-<input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
-<input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
+Name <input type="text"
+name="name" id="name"/></br>
+Email <input type="text"
+name="email" id="email"/></br>
 
-<input type ="text" name ="country" id ="country" placeholder ="Страна">
-
-<div>
-<input type ="submit" name ="submit" class ="btn" value ="Отправить"> 
-  <input type="submit" name="filter" class="btn" value="Фильтр">
 
 
 
